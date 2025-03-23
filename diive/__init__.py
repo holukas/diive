4271f22482af5e4f3578253c7f59001a9ee517e2
Lukas Hörtnagl <31772692+holukas@users.noreply.github.com>
2025-03-23 23:04:46 +0100

    Calculate monthly aggregation matrix

--- diive/__init__.py
+++ diive/__init__.py
@@ -1,6 +1,7 @@
 from diive.core.plotting.heatmap_datetime import HeatmapDateTime as heatmapdatetime
 from diive.core.plotting.heatmap_datetime import HeatmapYearMonth as heatmapyearmonth
 from diive.core.plotting.ridgeline import RidgeLinePlot as ridgeline
+from diive.core.times.resampling import resample_to_monthly_agg_matrix as resample_to_monthly_agg_matrix
 
 # import diive.core.plotting as plot
 # plot.ridgeline()
