bcbc8c69ac723bca6730e2ca75c2cdce64e3e49a
Lukas Hörtnagl <lukas.hoertnagl@usys.ethz.ch>
2025-03-24 12:15:41 +0100

    Update test_resampling.py

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -1,10 +1,23 @@
 import unittest
-
+import diive as dv
 import diive.configs.exampledata as ed
 from diive.core.times.resampling import diel_cycle
-
+from diive.configs.exampledata import load_exampledata_parquet_long
 
 class TestResampling(unittest.TestCase):
+
+    def test_resample_to_monthly_agg_matrix(self):
+        df = load_exampledata_parquet_long()
+        series = df['Tair_f'].copy()
+        monthly_means = dv.resample_to_monthly_agg_matrix(series=series, agg='mean', ranks=False)
+        monthly_means_ranks = dv.resample_to_monthly_agg_matrix(series=series, agg='mean', ranks=True)
+        self.assertEqual(monthly_means.shape, (26, 12))
+        self.assertEqual(monthly_means.loc[1997, 3], 1.083760752688172)
+        self.assertEqual(monthly_means.loc[2019, 6], 14.38481597222222)
+        self.assertEqual(monthly_means_ranks.loc[1997, 3], 5)
+        self.assertEqual(monthly_means_ranks.loc[2019, 6], 2)
+        self.assertEqual(monthly_means.sum().sum(), 1335.6155073307543)
+        self.assertEqual(monthly_means_ranks.sum().sum(), 4212)
 
     def test_diel_cycle(self):
         df = ed.load_exampledata_parquet()
