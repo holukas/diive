1f5cba2e4f57f8e69843315915e6de99083f5fcc
Lukas Hörtnagl <lukas.hoertnagl@usys.ethz.ch>
2026-04-17 14:51:33 +0200

    Fix: Correct hexbin_plot import path in test
    Update import to use correct module name 'hexbin_plot' instead of 'hexbin'
    to match actual file location. Fixes test collection error.

--- tests/test_hexbin_plot.py
+++ tests/test_hexbin_plot.py
@@ -4,7 +4,7 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from diive.core.plotting.hexbin import HexbinPlot
+from diive.core.plotting.hexbin_plot import HexbinPlot
 
 
 class TestHexbinPlot(unittest.TestCase):
